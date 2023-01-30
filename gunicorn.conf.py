c4e0efcf4bc7bf07bf3f56b28c077925c07c8987
daiki.tabata <daiki.tabata@datafluct.com>
2023-01-30 23:41:05 +0900

    fix gunicorn

---
+++ gunicorn.conf.py
@@ -0,0 +1,17 @@
+import psutil
+
+bind = '0.0.0.0:8000'
+worker_class = 'uvicorn.workers.UvicornH11Worker'
+
+workers = 2 * psutil.cpu_count(logical=True) + 1
+
+# https://github.com/benoitc/gunicorn/pull/862#issuecomment-53175919
+max_requests = 500
+max_requests_jitter = 200
+
+# https://cloud.google.com/load-balancing/docs/https#timeouts_and_retries
+keepalive = 620
+
+loglevel = 'debug'
+
+reload = True
